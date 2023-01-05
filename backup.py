1f26e3bdc533db307041df687e23b4549e0bf200
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-05 20:11:32 +0530

    Test
    [1] Add logger to log any messages or errors instead of printing them to the console.
    [2] Instead of using the `os.walk` function to recursively iterate over all the files in a directory, use the `glob` module to get a list of all the files in the directory.
    [3] Include docstrings.
    [4] Use the `with` statement to open and close the "token.json" file, which will automatically close the file when you are done with it. This will ensure that the file is closed even if an exception is raised while the file is open.

--- backup.py
+++ backup.py
@@ -1,6 +1,8 @@
 import json
 import os
 import os.path
+import glob
+import logging
 from datetime import datetime
 
 import google.auth
@@ -12,64 +14,106 @@ from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-creds = None
 
-try:
-    with open("token.json", "r") as token:
-        info = json.loads(token.read())
-        creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
-            info=info, scopes=SCOPES)
-except FileNotFoundError:
-    flow = google.auth.flow.InstalledAppFlow.from_client_secrets_file(
-        "credentials.json", SCOPES)
-    creds = flow.run_local_server(port=0)
+def create_folder(service, folder_name):
+    """
+    Creates a folder with the given name in Google Drive.
+    Returns the folder's ID.
+    """
+    file_metadata = {
+        "name": folder_name,
+        "mimeType": "application/vnd.google-apps.folder"
+    }
 
-    with open("token.json", "w") as token:
-        token.write(json.dumps(creds.to_json()))
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    folder_id = file.get("id")
+    return folder_id
 
-try:
-    service = build("drive", "v3", credentials=creds)
+
+def get_folder(service, folder_name):
+    """
+    Returns the ID of the folder with the given name if it exists in Google Drive,
+    otherwise creates the folder and returns the ID.
+    """
     response = service.files().list(
-        q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
+        q=f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder'",
+        spaces="drive"
+    ).execute()
     if not response["files"]:
-        file_metadata = {
-            "name": "Desktop Backup",
-            "mimeType": "application/vnd.google-apps.folder"
-        }
-
-        file = service.files().create(body=file_metadata, fields="id").execute()
-        folder_id = file.get("id")
+        folder_id = create_folder(service, folder_name)
     else:
         folder_id = response["files"][0]["id"]
+    return folder_id
 
+
+def create_subfolder(service, parent_folder_id, subfolder_name):
+    """
+    Creates a subfolder with the given name within the given parent folder in Google Drive.
+    Returns the subfolder's ID.
+    """
+    file_metadata = {
+        "name": subfolder_name,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [parent_folder_id]
+    }
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    subfolder_id = file.get("id")
+    return subfolder_id
+
+
+def upload_file(service, file_path, file_name, parent_folder_id):
+    """
+    Uploads the file at the given file path to the given parent folder in Google Drive.
+    """
+    file_metadata = {
+        "name": file_name,
+        "parents": [parent_folder_id]
+    }
+    media = MediaFileUpload(file_path)
+    upload_file = service.files().create(body=file_metadata,
+                                         media_body=media,
+                                         fields="id").execute()
+    logging.info(f"Backed up file: {file_name}")
+
+
+def main():
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Check for authorization file
+    creds = None
     try:
-        for root, dirs, files in os.walk("C:\\Users\\Sakshi\\Desktop"):
-            now = datetime.now()
-            date_time = now.strftime("%d-%m-%Y %H:%M:%S")
-            file_metadata = {
-                "name": date_time,
-                "mimeType": "application/vnd.google-apps.folder",
-                "parents": [folder_id]
-            }
-            file = service.files().create(body=file_metadata, fields="id").execute()
-            subfolder_id = file.get("id")
-
-            for file in files:
-                if file.endswith((".ini", ".lnk")):
-                    continue
-                file_metadata = {
-                    "name": file,
-                    "parents": [subfolder_id]
-                }
-                media = MediaFileUpload(os.path.join(root, file))
-                upload_file = service.files().create(body=file_metadata,
-                                                     media_body=media,
-                                                     fields="id").execute()
-                print(f"Backed up file: {file}")
+        with open("token.json", "r") as token:
+            info = json.loads(token.read())
+            creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
+                info=info, scopes=SCOPES)
     except FileNotFoundError:
-        print("The specified user was not found on the system.")
-except HttpError as e:
-    print(f"Error: {e}")
-finally:
-    with open("token.json", "r") as token:
-        token.close()
+        flow = google.auth.flow.InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        with open("token.json", "w") as token:
+            token.write(json.dumps(creds.to_json()))
+
+    # Create the Drive API service
+    service = build("drive", "v3", credentials=creds)
+
+    # Get or create the "Desktop Backup" folder
+    folder_id = get_folder(service, "Desktop Backup")
+
+    # Create a subfolder with the current date and time
+    now = datetime.now()
+    date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+    subfolder_id = create_subfolder(service, folder_id, date_time)
+
+    # Upload the files from the user's desktop to the subfolder
+    desktop_path = "C:\\Users\\Sakshi\\Desktop"
+    for file_path in glob.glob(os.path.join(desktop_path, "*")):
+        if file_path.endswith((".ini", ".lnk")):
+            continue
+        file_name = os.path.basename(file_path)
+        upload_file(service, file_path, file_name, subfolder_id)
+
+
+if __name__ == "__main__":
+    main()
