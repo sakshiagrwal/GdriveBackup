f99df0810d94b1fabcbbb0387fc5bc46c56e6185
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-05 23:19:34 +0530

    3

--- backup.py
+++ backup.py
@@ -1,7 +1,6 @@
 import json
 import os
 import os.path
-from datetime import datetime
 
 import google.auth
 from google.auth.transport.requests import Request
@@ -56,20 +55,34 @@ else:
     # Get the ID of the "Desktop Backup" folder
     folder_id = response["files"][0]["id"]
 
+# Dictionary to store the mapping of local paths to Google Drive folder IDs
+path_id_map = {}
+
 # Iterate over all files and directories in the user's desktop
 for root, dirs, files in os.walk(desktop_path):
-    # Get the current time
-    now = datetime.now()
-    date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+    # Get the relative path of the current directory with respect to the user's desktop
+    rel_path = os.path.relpath(root, desktop_path)
 
-    # Create a subfolder with the current date and time
-    file_metadata = {
-        "name": date_time,
-        "mimeType": "application/vnd.google-apps.folder",
-        "parents": [folder_id]
-    }
-    file = service.files().create(body=file_metadata, fields="id").execute()
-    subfolder_id = file.get("id")
+    # Check if the current directory is the user's desktop
+    if rel_path == ".":
+        # Use the "Desktop Backup" folder as the parent folder
+        parent_folder_id = folder_id
+    else:
+        # Check if a subfolder for the current directory has already been created in Google Drive
+        if rel_path in path_id_map:
+            # Use the existing subfolder as the parent folder
+            parent_folder_id = path_id_map[rel_path]
+        else:
+            # Create a subfolder for the current directory
+            file_metadata = {
+                "name": os.path.basename(rel_path),
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [folder_id]
+            }
+            file = service.files().create(body=file_metadata, fields="id").execute()
+            parent_folder_id = file.get("id")
+            # Store the mapping of the local path to the Google Drive folder ID
+            path_id_map[rel_path] = parent_folder_id
 
     # Iterate over all files in the current directory
     for file in files:
@@ -80,7 +93,7 @@ for root, dirs, files in os.walk(desktop_path):
         # Create a file metadata object
         file_metadata = {
             "name": file,
-            "parents": [subfolder_id]
+            "parents": [parent_folder_id]
         }
 
         # Create a MediaFileUpload object for the file
