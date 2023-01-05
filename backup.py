8cd0ff48bdd3fc5db860c918a27975a328318346
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-05 23:04:21 +0530

    1

--- backup.py
+++ backup.py
@@ -10,66 +10,89 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 
+# List of Google Drive API scope strings
 SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+# Path to the user's desktop
 desktop_path = os.path.join(os.environ['userprofile'], 'Desktop')
+
+# Credentials object to authenticate with the Google Drive API
 creds = None
 
-try:
+# Check if "token.json" exists
+if os.path.exists("token.json"):
+    # Read the contents of "token.json"
     with open("token.json", "r") as token:
         info = json.loads(token.read())
         creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
             info=info, scopes=SCOPES)
-except FileNotFoundError:
+else:
+    # Create a flow object to request user authorization
     flow = google.auth.flow.InstalledAppFlow.from_client_secrets_file(
         "credentials.json", SCOPES)
     creds = flow.run_local_server(port=0)
 
+    # Write the authorization information to "token.json"
     with open("token.json", "w") as token:
         token.write(json.dumps(creds.to_json()))
 
+# Create a Drive API service object
+service = build("drive", "v3", credentials=creds)
+
+# Query for the "Desktop Backup" folder
+response = service.files().list(
+    q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
+
+# Check if the "Desktop Backup" folder exists
+if not response["files"]:
+    # Create the "Desktop Backup" folder
+    file_metadata = {
+        "name": "Desktop Backup",
+        "mimeType": "application/vnd.google-apps.folder"
+    }
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    folder_id = file.get("id")
+else:
+    # Get the ID of the "Desktop Backup" folder
+    folder_id = response["files"][0]["id"]
+
+# Iterate over all files and directories in the user's desktop
 try:
-    service = build("drive", "v3", credentials=creds)
-    response = service.files().list(
-        q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
-    if not response["files"]:
+    for root, dirs, files in os.walk(desktop_path):
+        # Get the current time
+        now = datetime.now()
+        date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+
+        # Create a subfolder with the current date and time
         file_metadata = {
-            "name": "Desktop Backup",
-            "mimeType": "application/vnd.google-apps.folder"
+            "name": date_time,
+            "mimeType": "application/vnd.google-apps.folder",
+            "parents": [folder_id]
         }
-
         file = service.files().create(body=file_metadata, fields="id").execute()
-        folder_id = file.get("id")
-    else:
-        folder_id = response["files"][0]["id"]
+        subfolder_id = file.get("id")
 
-    try:
-        for root, dirs, files in os.walk(desktop_path):
-            now = datetime.now()
-            date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+        # Iterate over all files in the current directory
+        for file in files:
+            # Skip certain file types
+            if file.endswith((".ini", ".lnk")):
+                continue
+
+            # Create a file metadata object
             file_metadata = {
-                "name": date_time,
-                "mimeType": "application/vnd.google-apps.folder",
-                "parents": [folder_id]
+                "name": file,
+                "parents": [subfolder_id]
             }
-            file = service.files().create(body=file_metadata, fields="id").execute()
-            subfolder_id = file.get("id")
 
-            for file in files:
-                if file.endswith((".ini", ".lnk")):
-                    continue
-                file_metadata = {
-                    "name": file,
-                    "parents": [subfolder_id]
-                }
-                media = MediaFileUpload(os.path.join(root, file))
-                upload_file = service.files().create(body=file_metadata,
-                                                     media_body=media,
-                                                     fields="id").execute()
-                print(f"Backed up file: {file}")
-    except FileNotFoundError:
-        print("The specified user was not found on the system.")
+            # Create a MediaFileUpload object for the file
+            media = MediaFileUpload(os.path.join(root, file))
+
+            # Use the Drive API to upload the file
+            upload_file = service.files().create(body=file_metadata,
+                                                 media_body=media,
+                                                 fields="id").execute()
+            print(f"Backed up file: {file}")
+except FileNotFoundError:
+    print("The specified user was not found on the system.")
 except HttpError as e:
     print(f"Error: {e}")
-finally:
-    with open("token.json", "r") as token:
-        token.close()
