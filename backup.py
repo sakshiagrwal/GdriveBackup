edd9ea3ba7137be5b296923b459e13a0ec6e890e
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-06 01:51:38 +0530

    Cleanup

--- backup.py
+++ backup.py
@@ -12,64 +12,54 @@ from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-creds = None
+# Read the token from "token.json"
+with open("token.json", "r") as token:
+    info = json.loads(token.read())
+    creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
+        info=info, scopes=SCOPES)
 
-try:
-    with open("token.json", "r") as token:
-        info = json.loads(token.read())
-        creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
-            info=info, scopes=SCOPES)
-except FileNotFoundError:
-    flow = google.auth.flow.InstalledAppFlow.from_client_secrets_file(
-        "credentials.json", SCOPES)
-    creds = flow.run_local_server(port=0)
+# Authenticate with the Google Drive API
+service = build("drive", "v3", credentials=creds)
 
-    with open("token.json", "w") as token:
-        token.write(json.dumps(creds.to_json()))
+# Search for the "Desktop Backup" folder
+response = service.files().list(
+    q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
 
-try:
-    service = build("drive", "v3", credentials=creds)
-    response = service.files().list(
-        q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
-    if not response["files"]:
+# If the folder doesn't exist, create it
+if not response["files"]:
+    file_metadata = {
+        "name": "Desktop Backup",
+        "mimeType": "application/vnd.google-apps.folder"
+    }
+
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    folder_id = file.get("id")
+else:
+    folder_id = response["files"][0]["id"]
+
+# Iterate through the files and subdirectories on the user's desktop
+for root, dirs, files in os.walk(os.path.join(os.environ['userprofile'], 'Desktop')):
+    now = datetime.now()
+    date_time = now.strftime("%d-%m-%Y %H:%M:%S")
+    file_metadata = {
+        "name": date_time,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [folder_id]
+    }
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    subfolder_id = file.get("id")
+
+    # Iterate through the files in the subdirectory
+    for file in files:
+        if file.endswith((".ini", ".lnk")):
+            continue
+
         file_metadata = {
-            "name": "Desktop Backup",
-            "mimeType": "application/vnd.google-apps.folder"
+            "name": file,
+            "parents": [subfolder_id]
         }
-
-        file = service.files().create(body=file_metadata, fields="id").execute()
-        folder_id = file.get("id")
-    else:
-        folder_id = response["files"][0]["id"]
-
-    try:
-        for root, dirs, files in os.walk(os.path.join(os.environ['userprofile'], 'Desktop')):
-            now = datetime.now()
-            date_time = now.strftime("%d-%m-%Y %H:%M:%S")
-            file_metadata = {
-                "name": date_time,
-                "mimeType": "application/vnd.google-apps.folder",
-                "parents": [folder_id]
-            }
-            file = service.files().create(body=file_metadata, fields="id").execute()
-            subfolder_id = file.get("id")
-
-            for file in files:
-                if file.endswith((".ini", ".lnk")):
-                    continue
-                file_metadata = {
-                    "name": file,
-                    "parents": [subfolder_id]
-                }
-                media = MediaFileUpload(os.path.join(root, file))
-                upload_file = service.files().create(body=file_metadata,
-                                                     media_body=media,
-                                                     fields="id").execute()
-                print(f"Backed up file: {file}")
-    except FileNotFoundError:
-        print("The specified user was not found on the system.")
-except HttpError as e:
-    print(f"Error: {e}")
-finally:
-    with open("token.json", "r") as token:
-        token.close()
+        media = MediaFileUpload(os.path.join(root, file))
+        upload_file = service.files().create(body=file_metadata,
+                                             media_body=media,
+                                             fields="id").execute()
+        print(f"Backed up: {file}")
