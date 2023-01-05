bd662dd9a4df24b281d1fd6e7aeb84bfa3a775bc
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-05 15:06:38 +0530

    Backup All
    Backup of all files and folders in the "C:\Users\Sakshi\Desktop" directory.
    
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- backup.py
+++ backup.py
@@ -1,9 +1,10 @@
+import json
 import os
 import os.path
 
+import google.auth
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
@@ -14,23 +15,24 @@ creds = None
 
 try:
     with open("token.json", "r") as token:
-        creds = Credentials.from_authorized_user_info(
-            info=token.read(), scopes=SCOPES)
+        info = json.loads(token.read())
+        creds = google.oauth2.credentials.Credentials.from_authorized_user_info(
+            info=info, scopes=SCOPES)
 except FileNotFoundError:
-    flow = InstalledAppFlow.from_client_secrets_file(
+    flow = google.auth.flow.InstalledAppFlow.from_client_secrets_file(
         "credentials.json", SCOPES)
     creds = flow.run_local_server(port=0)
 
     with open("token.json", "w") as token:
-        token.write(creds.to_json())
+        token.write(json.dumps(creds.to_json()))
 
 try:
     service = build("drive", "v3", credentials=creds)
     response = service.files().list(
-        q="name='MasterBackup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
+        q="name='Desktop Backup' and mimeType='application/vnd.google-apps.folder'", spaces="drive").execute()
     if not response["files"]:
         file_metadata = {
-            "name": "MasterBackup",
+            "name": "Desktop Backup",
             "mimeType": "application/vnd.google-apps.folder"
         }
 
@@ -38,12 +40,14 @@ try:
         folder_id = file.get("id")
     else:
         folder_id = response["files"][0]["id"]
-        for file in os.listdir(os.path.join("backupfiles")):
+
+    for root, dirs, files in os.walk("C:\\Users\\Sakshi\\Desktop"):
+        for file in files:
             file_metadata = {
                 "name": file,
                 "parents": [folder_id]
             }
-            media = MediaFileUpload(f"{os.path.join('backupfiles', file)}")
+            media = MediaFileUpload(os.path.join(root, file))
             upload_file = service.files().create(body=file_metadata,
                                                  media_body=media,
                                                  fields="id").execute()
