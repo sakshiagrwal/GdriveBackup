c2ab698a8c9383ba6a0f422a3fc7992af69fb905
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-03-02 21:22:00 +0530

    wip

--- backup.py
+++ backup.py
@@ -1,61 +1,60 @@
 import os
 import shutil
+import logging
 from datetime import datetime
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
-def create_zip(path, file_name):
-    desktop_path = os.path.join(os.path.expandvars("%userprofile%"), "Desktop")
-    backup_dir = os.path.join(desktop_path, "backup_archive")
-    os.makedirs(backup_dir, exist_ok=True)
+BACKUP_DIR = os.path.join(os.path.expandvars("%userprofile%"), "Desktop", "backup")
+ARCHIVE_DIR = os.path.join(os.path.expandvars("%userprofile%"), "Desktop", "backup_archive")
+CREDENTIALS_FILE = "credentials.json"
+
+
+def create_zip(source_dir, dest_dir, file_name):
     try:
-        shutil.make_archive(os.path.join(backup_dir, file_name), "zip", path)
-        return True
-    except FileNotFoundError as e:
-        return False
+        os.makedirs(dest_dir, exist_ok=True)
+        archive_path = os.path.join(dest_dir, file_name)
+        shutil.make_archive(archive_path, "zip", source_dir)
+        return archive_path + ".zip"
+    except FileNotFoundError:
+        logging.error(f"Failed to create ZIP file: {source_dir} not found")
+        return None
 
 
-def google_auth():
+def authenticate_drive():
     gauth = GoogleAuth()
-    gauth.LoadCredentialsFile("credentials.json")
+    gauth.LoadCredentialsFile(CREDENTIALS_FILE)
     if gauth.credentials is None:
         gauth.LocalWebserverAuth()
-        gauth.SaveCredentialsFile("credentials.json")
+        gauth.SaveCredentialsFile(CREDENTIALS_FILE)
     elif gauth.access_token_expired:
         gauth.Refresh()
     else:
         gauth.Authorize()
     drive = GoogleDrive(gauth)
-    return gauth, drive
+    return drive
 
 
-def upload_backup(drive, path, file_name):
-    f = drive.CreateFile({"title": file_name})
-    f.SetContentFile(os.path.join(path, file_name))
-    f.Upload()
-    f = None
+def upload_file(drive, file_path, file_name):
+    file_metadata = {"title": file_name}
+    media = GoogleDrive.CreateFile(file_metadata)
+    media.SetContentFile(file_path)
+    media.Upload()
+    logging.info(f"Uploaded {file_name} to Google Drive")
 
 
-def controller():
-    path = os.path.join(os.path.expandvars("%userprofile%"), "Desktop", "backup")
+def backup_and_upload():
     now = datetime.now()
-    file_name = "backup " + now.strftime(r"%d-%m-%Y %H:%M:%S").replace(
-        ":", "-"
-    ).replace(" ", "_")
-
-    if not create_zip(path, file_name):
-        print("Failed to create ZIP file")
+    file_name = f"backup_{now.strftime('%d-%m-%Y_%H-%M-%S')}.zip"
+    archive_path = create_zip(BACKUP_DIR, ARCHIVE_DIR, file_name)
+    if not archive_path:
         return
 
-    auth, drive = google_auth()
-    upload_backup(
-        drive,
-        os.path.join(os.environ["USERPROFILE"], "Desktop", "backup_archive"),
-        file_name + ".zip",
-    )
-    print("Backup uploaded successfully")
+    drive = authenticate_drive()
+    upload_file(drive, archive_path, file_name)
 
 
 if __name__ == "__main__":
-    controller()
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    backup_and_upload()
