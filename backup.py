8cfdb3e5cb35dfcc4e8a0b6fdb35c30db20b9024
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-03-02 23:32:14 +0530

    log for every step

--- backup.py
+++ backup.py
@@ -13,11 +13,14 @@ FOLDER_NAME = "Vivobook"
 
 
 def create_zip(source_dir, dest_dir, file_name):
+    logging.info(f"Creating ZIP file from {source_dir} to {dest_dir} with name {file_name}")
     try:
         os.makedirs(dest_dir, exist_ok=True)
         file_name_without_ext = os.path.splitext(file_name)[0]
         archive_path = os.path.join(dest_dir, file_name_without_ext)
         shutil.make_archive(archive_path, "zip", source_dir)
+        zip_file_path = archive_path + ".zip"
+        logging.info(f"Created ZIP file: {zip_file_path}")
         return zip_file_path
     except FileNotFoundError:
         logging.error(f"Failed to create ZIP file: {source_dir} not found")
@@ -25,6 +28,7 @@ def create_zip(source_dir, dest_dir, file_name):
 
 
 def authenticate_drive():
+    logging.info("Authenticating Google Drive API...")
     gauth = GoogleAuth()
     gauth.LoadCredentialsFile(CREDENTIALS_FILE)
     if gauth.credentials is None:
@@ -34,10 +38,12 @@ def authenticate_drive():
         gauth.Refresh()
     else:
         gauth.Authorize()
+    logging.info("Authenticated Google Drive API")
     return gauth
 
 
 def upload_file(gauth, file_path, file_name):
+    logging.info(f"Uploading file {file_name} to Google Drive...")
     drive = GoogleDrive(gauth)
     query = f"title='{FOLDER_NAME}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
     folder_list = drive.ListFile({'q': query}).GetList()
@@ -69,6 +75,7 @@ def backup_and_upload():
         upload_file(gauth, archive_path, file_name)
     except Exception as e:
         logging.error(f"Failed to upload {file_name} to Google Drive: {str(e)}")
+        return
     finally:
         shutil.rmtree(ARCHIVE_DIR)
         logging.info(f"Removed archive directory: {ARCHIVE_DIR}")
